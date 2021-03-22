6a079f2c416ed25dc2f558c5620c6dcc9000f4e5
Bee Kay <beekayuni@gmail.com>
2021-03-22 10:34:11 +0000

    add startGame function in js

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,3 +1,7 @@
 const startButtonElement = document.getElementById("start-btn");
 const timerSpanElement = document.getElementById("timer");
 const bodyElement = document.body;
+
+const startGame = () => {};
+
+startButtonElement.addEventListener("click", startGame);
